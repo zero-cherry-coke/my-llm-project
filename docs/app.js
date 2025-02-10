2fec8a631a907e32325b603cfd798ae5e1109de1
dochmai382 <dochmai382@gmail.com>
2025-02-10 17:23:47 +0900

    final

--- docs/app.js
+++ docs/app.js
@@ -1,30 +1,48 @@
 async function main() {
-  async function handleRecommend(event) {
-    event.preventDefault(); // form의 기본 submit 막아줘야하고
+  async function hanldeCC(event) {
+    event.preventDefault(); // Form의 기본 submit 막아줘야하고...
 
-    const url = "http://127.0.0.1:3000";
+    // 로딩을 추가했다가...
+    const spinner = document.createElement("div");
+    spinner.classList.add("spinner-border");
+    document.querySelector("#box").appendChild(spinner);
+
+    const url = "https://platinum-sweltering-birthday.glitch.me";
     const formData = new FormData(document.querySelector("#ccForm"));
     const text = formData.get("text");
     // console.log(text);
-    const response = await await fetch(url, {
+    const response = await fetch(url, {
       method: "POST",
       body: JSON.stringify({
         text,
       }),
-      // Content-Type 꼭!!!
+      // Content-Type 꼭!
       headers: {
-        "Content-Type": "application/json",
+        "Content-Type": "Application/json",
       },
     });
     const json = await response.json();
 
-    document.querySelector("#box").textContent = JSON.stringify(json);
+    // 로딩을 추가했다가 종료시 없애는...
+    spinner.remove();
+
+    // document.querySelector("#box").textContent = JSON.stringify(json);
+
+    const { image, desc } = json;
+
+    const box = document.querySelector("#box");
+    box.innerHTML = "";
+    const imageTag = document.createElement("img");
+    imageTag.classList.add("img-fluid");
+    imageTag.src = image; // image - link
+    const descTag = document.createElement("p");
+    descTag.textContent = desc;
+    box.appendChild(imageTag);
+    box.appendChild(descTag);
   }
 
-  //   document
-  //     .querySelector("#recommendBtn")
-  //     .addEventListener("click", handleRecommend);
-  document.querySelector("#ccForm").addEventListener("submit", handleRecommend);
+  //   document.querySelector("#ccBtn").addEventListener("click", hanldeCC);
+  document.querySelector("#ccForm").addEventListener("submit", hanldeCC);
 }
 
 document.addEventListener("DOMContentLoaded", main);
